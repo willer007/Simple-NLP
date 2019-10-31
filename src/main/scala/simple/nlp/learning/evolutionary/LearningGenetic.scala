1349bc003805cc0e75ddd7830b0baefe9b912fe0
unknown <wilquintanilha@gmail.com>
2019-10-31 12:35:09 -0300

    efetuando correcoes nos algoritimos de aprendizado

--- src/main/scala/simple/nlp/learning/evolutionary/LearningGenetic.scala
+++ src/main/scala/simple/nlp/learning/evolutionary/LearningGenetic.scala
@@ -6,53 +6,52 @@ import scala.util.Random
 
 object LearningGenetic {
   var generationSize = 9
-  var cromossomeSize = 0
+  var chromosomeSize = 0
 
   var generation: Array[Array[Float]] = Array.empty
-  var best: Array[Array[Float]] = Array.empty
   var weights: Array[Float] = Array.empty
 
   def initModel() = {
     generation = createGen()
-    best = createGen()
-    weights = createChromossome()
+    weights = createChromosome()
 
   }
 
-  def optimize(objective: Int, tokens: Array[Float], iterations: Int): Array[Array[Float]] = {
+  def optimize(objective: Int, input: Array[Float], iterations: Int): Array[Float] = {
 
     //CONCERTAR ESTA PARTE
     for (i <- 0 to iterations) {
-      best = Array.empty
-      var auxMin: Array[Float] = Array.empty
-      for (chromosome <- generation) {
-        auxMin = auxMin :+ math.abs(OperatorArray.linearCombination(chromosome, tokens))
-      }
 
-      var auxSorted = (generation zip auxMin).sortBy(t => math.abs(objective * 10 - t._2))
 
-      weights = auxSorted(0)._1
+     val chromossomeSortedByObjective = generation
+        .map(chromosome => ( chromosome, OperatorArray.linearCombination(chromosome, input)))
+          .sortBy(t => math.abs(objective - t._2))
+
+
+      weights = chromossomeSortedByObjective(0)._1
 
       //0 ate 3 inclusivo
-      best = auxSorted.slice(0, 3).map(g => g._1)
+      var best: Array[Array[Float]] = Array.empty
+      best = chromossomeSortedByObjective.slice(0, 3).map(g => g._1)
       best = best :+ crossover(best(0), best(1))
       best = best :+ mutation(best(0))
       best = best :+ mutation(best(1))
       best = best :+ mutation(best(2))
-      best = best :+ createChromossome()
+      best = best :+ createChromosome()
+      best = best :+ createChromosome()
 
 
       generation = best
-      println("GERACAO: \n" + generation(0).slice(0, 10).map(a => a.toString).reduce((a, b) => a + " \n" + b))
-      println("PESOS: \n" + weights.slice(0, 10).map(a => a.toString).reduce((a, b) => a + " \n " + b))
-      println("PESO ITERACAO " + i + ": " + math.abs(objective - OperatorArray.linearCombination(tokens,weights).floatValue()))
+  //    println("GERACAO: \n" + generation(0).slice(0, 10).map(a => a.toString).reduce((a, b) => a + " \n" + b))
+  //    println("PESOS: \n" + weights.slice(0, 10).map(a => a.toString).reduce((a, b) => a + " \n " + b))
+  //    println("PESO ITERACAO " + i + ": " + math.abs(objective - OperatorArray.linearCombination(input,weights).floatValue()))
       println("\n")
 
     }
 
 
-    print("PESO FINAL: " + OperatorArray.linearCombination(tokens ,weights).floatValue())
-    return best
+    print("PESO FINAL: " + OperatorArray.linearCombination(input ,weights).floatValue())
+    return weights
   }
 
   def objectiveFunction(sumValue: Float, obj: Float): Float = math.abs(sumValue - obj)
@@ -69,7 +68,7 @@ object LearningGenetic {
   //MUTCAO DE 10% ESTA OK
   def mutation(cromossome: Array[Float]): Array[Float] = {
     for (_ <- 0 to cromossome.length / 10) {
-      cromossome(Random.nextInt(cromossomeSize)) = Random.nextFloat()
+      cromossome(Random.nextInt(chromosomeSize)) = Random.nextFloat()
     }
     cromossome
   }
@@ -77,12 +76,12 @@ object LearningGenetic {
 
   //FUNCAO ESTA OK
   def createGen(): Array[Array[Float]] = Array.fill(generationSize) {
-    createChromossome()
+    createChromosome()
   }
 
 
   //FUNCAO ESTA OK
-  def createChromossome(): Array[Float] = Array.fill(cromossomeSize) {
+  def createChromosome(): Array[Float] = Array.fill(chromosomeSize) {
     Random.nextFloat()
   }
 
--- src/main/scala/simple/nlp/learning/evolutionary/LearningGeneticTest.scala
+++ src/main/scala/simple/nlp/learning/evolutionary/LearningGeneticTest.scala
@@ -4,9 +4,9 @@ object LearningGeneticTest {
 
   def main(args: Array[String]): Unit = {
     LearningGenetic.generationSize = 9
-    LearningGenetic.cromossomeSize = 10
+    LearningGenetic.chromosomeSize = 10
     LearningGenetic.initModel()
-    var teste = LearningGenetic.optimize(1,Array(1,5,6,5,7,4,8,1,4,3),200);
+    var teste = LearningGenetic.optimize(1,Array(1,5,6,5,7,4,8,1,4,3),1000);
 
     print("teste")
 
