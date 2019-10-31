c5919304322169700148543711509dae0c22ae1a
unknown <wilquintanilha@gmail.com>
2019-10-31 16:06:29 -0300

    adicionado método de otimização para n entradas

--- src/main/scala/simple/nlp/learning/evolutionary/LearningGenetic.scala
+++ src/main/scala/simple/nlp/learning/evolutionary/LearningGenetic.scala
@@ -7,77 +7,129 @@ import scala.util.Random
 object LearningGenetic {
   private var generationSize = 0
   private var chromosomeSize = 0
-  private var mutation = 10;
+  private var elitismSize = 3;
+  private var crossoverSize = 2;
+  private var mutationChance = 10;
 
 
-  def initModel(generationSize : Int , chromosomeSize:Int ) = {
+  def initModel(generationSize: Int, chromosomeSize: Int) = {
     this.generationSize = generationSize
     this.chromosomeSize = chromosomeSize
   }
 
 
+  def processNextGeneration( chromossomeSortedByObjective:Array[(Array[Float],Float)] ):Array[Array[Float]] = {
+    var nextGeneration: Array[Array[Float]] = Array.empty
+
+    val elitismIndividuals =
+      chromossomeSortedByObjective.slice(0, elitismSize).map(cromossome => cromossome._1)
+
+    nextGeneration = nextGeneration ++ elitismIndividuals
+
+    for (elitismIndividual <- elitismIndividuals) {
+      nextGeneration = nextGeneration :+ mutation(elitismIndividual)
+    }
+
+    for (index <- 0 until crossoverSize){
+      nextGeneration = nextGeneration :+ crossover(nextGeneration(index), nextGeneration(index + 1))
+    }
+
+    for (index <- 0 until  generationSize - nextGeneration.length ) {
+      nextGeneration = nextGeneration :+ createChromosome()
+    }
+
+    return nextGeneration
+
+  }
+
   def optimize(objective: Int, input: Array[Float], iterations: Int): Array[Float] = {
 
     var generation: Array[Array[Float]] = createGen()
     var weights: Array[Float] = createChromosome()
+    var prediction = 0.0
 
     for (i <- 0 to iterations) {
 
-
-     val chromossomeSortedByObjective = generation
-       .map(chromosome => ( chromosome, OperatorArray.linearCombination(chromosome, input)))
-       .sortBy(t => math.abs(objective - t._2))
-       .map(t => t._1)
+      val chromossomeSortedByObjective = generation
+        .map(chromosome => (chromosome, OperatorArray.linearCombination(chromosome, input)))
+        .sortBy(t => math.abs(objective - t._2))
 
 
-      weights = chromossomeSortedByObjective(0)
+      weights = chromossomeSortedByObjective(0)._1
+      prediction = chromossomeSortedByObjective(0)._2
 
-      var best: Array[Array[Float]] = Array.empty
-      best = chromossomeSortedByObjective.slice(0, 3)
-      best = best :+ mutation(best(0))
-      best = best :+ mutation(best(1))
-      best = best :+ mutation(best(2))
-      best = best :+ crossover(best(0), best(1))
-      best = best :+ crossover(best(1), best(2))
-      best = best :+ createChromosome()
-      generation = best
-      println("ERROR ITERATION " + i + ": " + math.abs(objective - OperatorArray.linearCombination(input,weights).floatValue()))
+      generation = processNextGeneration(chromossomeSortedByObjective)
+      
+      println("ERROR ITERATION " + i + ": " + math.abs(objective - prediction))
     }
 
-    print("WEIGHTS * INPUT =  " + OperatorArray.linearCombination(input ,weights).floatValue())
+    print("WEIGHTS * INPUT =  " + prediction)
     return weights
   }
+
+
+  def optimize(objective: Int, input: Array[Array[Float]], iterations: Int): Array[Float] = {
+
+    var generation: Array[Array[Float]] = createGen()
+    var weights: Array[Float] = createChromosome()
+    var prediction = 0.0
+
+    for (i <- 0 to iterations) {
+
+      val chromossomeSortedByObjective = generation
+        .map(chromosome => (chromosome, input
+          .map(i => OperatorArray.linearCombination(chromosome, i)).sum))
+        .sortBy(t => math.abs(objective - t._2))
+
+      weights = chromossomeSortedByObjective(0)._1
+      prediction = chromossomeSortedByObjective(0)._2
+
+      generation = processNextGeneration(chromossomeSortedByObjective)
+
+      println("ERROR ITERATION " + i + ": " + math.abs(objective - prediction))
+    }
+
+    print("WEIGHTS * INPUT =  " + prediction)
+    return weights
+  }
+
 
   def objectiveFunction(sumValue: Float, obj: Float): Float = math.abs(sumValue - obj)
 
 
-  //CROSSOVER PELA METADE DO CROMOSSOMO ESTA OK
   def crossover(cromossome1: Array[Float], cromossome2: Array[Float]) =
     cromossome1.slice(0, cromossome1.length / 2) ++ cromossome2.slice(cromossome1.length / 2, cromossome1.length)
 
 
-  //MUTACAO DE 10% ESTA OK
   def mutation(cromossome: Array[Float]): Array[Float] = {
-    var chromossomeAux:Array[Float] =  cromossome.map( a => a)
-    for (_ <- 0 to cromossome.length / mutation) {
+    var chromossomeAux: Array[Float] = cromossome.map(a => a)
+    for (_ <- 0 to cromossome.length / mutationChance) {
       chromossomeAux(Random.nextInt(chromosomeSize)) = Random.nextFloat()
     }
     chromossomeAux
   }
 
-
-  //FUNCAO ESTA OK
   def createGen(): Array[Array[Float]] = Array.fill(generationSize) {
     createChromosome()
   }
 
-  //FUNCAO ESTA OK
   def createChromosome(): Array[Float] = Array.fill(chromosomeSize) {
     Random.nextFloat()
   }
 
-  def setMutation(mutationChance:Int) = this.mutation = mutationChance
+  def setMutationChance(mutationChance: Int) = this.mutationChance = mutationChance
 
-  def getMutation() = this.mutation
+  def getMutationChance() = this.mutationChance
+
+  def setElitismSize(elitismSize: Int) = this.elitismSize = elitismSize
+
+  def getElitismSize(selection: Int): Int = this.elitismSize
+
+  def setCrossoverSize(crossover:Int) = this.crossoverSize = crossoverSize
+
+  def getCrossoverSize() = this.crossoverSize
+
+
+
 
 }
--- src/main/scala/simple/nlp/learning/evolutionary/LearningGeneticTest.scala
+++ src/main/scala/simple/nlp/learning/evolutionary/LearningGeneticTest.scala
@@ -4,7 +4,20 @@ object LearningGeneticTest {
 
   def main(args: Array[String]): Unit = {
     LearningGenetic.initModel(9,10)
-    var teste = LearningGenetic.optimize(1,Array(1,5,6,5,7,4,8,1,4,3),200);
+
+    //MUTATION IN 10%
+    LearningGenetic.setMutationChance(10)
+
+    //MANTAIN BEST 3 INDIVIDUALS IN GENERATION
+    LearningGenetic.setElitismSize(3)
+
+    //EXECUTE 2 CROSSOVER WITH THE ELIT
+    LearningGenetic.setCrossoverSize(2)
+
+
+
+    val input:Array[Float] = Array(1,5,6,5,7,4,8,1,4,3)
+    var teste = LearningGenetic.optimize(1,input,200);
 
 
   }
