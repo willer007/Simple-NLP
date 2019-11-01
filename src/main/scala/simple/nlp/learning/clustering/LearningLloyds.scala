68aee4cb2a04d3ee0d98535b27b09bab28d4fb91
unknown <wilquintanilha@gmail.com>
2019-11-01 17:40:36 -0300

    algoritimo de lloyd refatorado

--- src/main/scala/simple/nlp/learning/clustering/LearningLloyds.scala
+++ src/main/scala/simple/nlp/learning/clustering/LearningLloyds.scala
@@ -1,29 +1,30 @@
 package simple.nlp.learning.clustering
 
 import simple.nlp.operator.OperatorArray
+
 import scala.collection.mutable.ArrayBuffer
-import scala.util.Random
 
 
 
+
+//CONCERTAR ESTE ALGORITIMO
 object LearningLloyds{
   private var clusters: Array[Cluster] = Array.empty
+  private var numOfDimension:Int = 0
+  private var numOfClusters:Int  =0
 
-  def initModel(numOfClusters: Int, sizeOfClusters:Int): Unit = {
+  def initModel(numOfClusters: Int, numOfDimension:Int): Unit = {
+    this.numOfClusters = numOfClusters
+    this.numOfDimension = numOfDimension
     for (i <- 1 to numOfClusters) {
-      clusters = Cluster.createCluster(sizeOfClusters) +: clusters
-    }
-  }
-
-  def initModel(arrayOfClusters:Array[Array[Float]]):Unit = {
-    for ( centroid <- arrayOfClusters) {
-      clusters = Cluster.createCluster(centroid) +: clusters
+      clusters = Cluster.createCluster(numOfDimension) +: clusters
     }
   }
 
   def distanceClusterFromData(cluster:Cluster, data:Array[Float]) : Float =
     OperatorArray.euclideanDistance(cluster.centroid,data)
 
+  //ASSIGN DATA TO CLUSTER WITH MINIMUN DISTANCE
   def assignDataToCluster(data:Array[Float]): Unit = {
     var clustersOrdenedByDistance = clusters
       .map(cluster => (cluster, distanceClusterFromData(cluster, data)))
@@ -32,12 +33,42 @@ object LearningLloyds{
     Cluster.addData(clustersOrdenedByDistance(0)._1,data)
   }
 
-  def optimize(inputs:Array[Array[Float]]): Array[Cluster] = {
+  def cleanClusterData(cluster:Cluster):Cluster = {
+    return Cluster(cluster.centroid, new ArrayBuffer[Array[Float]])
+  }
 
-    /*clusters = clusters.map(c => Cluster.setCentroid(c, optimizeCluster(c)))
+  //CACULETE THE MEAN OF ALL POINTS IN THE CLUSTERS
+  def calculeClusterMean(cluster: Cluster):Array[Float] = {
+    if (cluster.data.isEmpty)
+      return Cluster.createCluster(numOfDimension).centroid
+
+    var clusterDataMean: Array[Float] =
+      cluster.data.reduce( (d1,d2) => OperatorArray.sumArrayCoordinates(d1,d2))
+      .map(k => k / cluster.data.length)
+
+    return clusterDataMean
+  }
 
 
-    return clusters*/
+
+  def optimize(inputs:Array[Array[Float]], iterations:Int): Array[Cluster] = {
+    inputs.foreach( d => assignDataToCluster(d))
+
+
+    //A COMPLETE ITERATION
+    // STEP CALCULE MEAN OF CLUSTER DATA
+    // ASSIGN MEAN TO CLUSTER CENTROID
+    // CLEAR CLUSTER DATA
+    // ASSIGN DATA TO NEW CLUSTERS WITH OPTIMIZED CENTROIDS
+    for( _ <- 1 to iterations) {
+      clusters = clusters
+        .map(c => Cluster.setCentroid(c, calculeClusterMean(c)))
+        .map(c => cleanClusterData(c))
+
+      inputs.foreach(d => assignDataToCluster(d))
+    }
+
+
     return clusters
   }
 
--- src/test/scala/nlp/learning/clustering/LearningLloydsTest.scala
+++ src/test/scala/nlp/learning/clustering/LearningLloydsTest.scala
@@ -5,8 +5,18 @@ import simple.nlp.learning.clustering.LearningLloyds
 object LearningLloydsTest {
 
   def main(args: Array[String]): Unit = {
-    LearningLloyds.initModel(10,10)
-    LearningLloyds.
+
+    LearningLloyds.initModel(10, 10)
+
+    var inputs: Array[Array[Float]] = Array.empty
+    for (num <- 1 to 10) {
+      inputs = inputs :+ Array.fill[Float](10)(num.toFloat)
+    }
+
+
+    var teste = LearningLloyds.optimize(inputs,100)
+
+    print("teste")
   }
 
 }
